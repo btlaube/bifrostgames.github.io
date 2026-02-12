9d096d2b5cc7598df14be7e53d35f661cd79cd86
btlaube <69732464+btlaube@users.noreply.github.com>
2026-02-12 10:04:35 -0600

    removed echoice test
    and archived click scroller game test

--- echoice/cart.js
+++
@@ -1,5 +0,0 @@
-function checkout(stripeLink) {
-  // Optional: store last product for thank-you page display
-  localStorage.setItem('lastProduct', stripeLink);
-  window.location.href = stripeLink;
-}
